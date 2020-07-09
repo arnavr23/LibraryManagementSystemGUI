34f17a47d8aa8944f54798896192afee6952a57f
arnavr23 <arnavvijaywargiya@gmail.com>
2020-07-09 22:15:49 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `java` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `java`;
 -- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: java
@@ -135,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-09 21:33:00
+-- Dump completed on 2020-07-09 22:13:56
